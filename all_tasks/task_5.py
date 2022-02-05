c618f921495fd3acbe33384bbb58cd7ecfd4c0e7
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-02-05 19:30:20 +0300

    Отбор астронавтов готова
    Всё.

---
+++ all_tasks/task_5.py
@@ -0,0 +1,49 @@
+import pathlib
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def index():
+    return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def promotion():
+    promotion_words = [
+        'Человечество вырастает из детства.',
+        'Человечеству мала одна планета.',
+        'Мы сделаем обитаемыми безжизненные пока планеты.',
+        'И начнем с Марса!', 'Присоединяйся!'
+    ]
+    return '<br>'.join(promotion_words)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return pathlib.Path('static/html/img_mars.html').read_text()
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return pathlib.Path('static/html/promotion_img.html').read_text()
+
+
+@app.route('/astronaut_selection')
+def astronaut_selection():
+    return pathlib.Path('static/html/form.html').read_text()
+
+
+def main():
+    app.run(host='', port=8080)
+
+
+if __name__ == '__main__':
+    main()
