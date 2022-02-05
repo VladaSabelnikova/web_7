5a99520607db9bea56b8fe48c5ffa67977054cf5
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-02-05 20:19:59 +0300

    Варианты выбора готова
    Всё.

---
+++ all_tasks/static/templates/temp.py
@@ -0,0 +1,19 @@
+proposal = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Варианты выбора</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="http://localhost:8080/static/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Моё предложение: {}!</h1>
+        <div class="col coral">Это красивая планета!</div>
+        <div class="col green">Это просто замечательная планета!</div>
+        <div class="col pink">Это очень перспективная для развития жизни планета!</div>
+    </div>
+</body>
+</html>
+"""
