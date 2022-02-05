ab73223540bda1295af1b1a64da1c56beea65f85
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-02-05 20:37:37 +0300

    Результат отбора готова
    Всё.

--- all_tasks/static/templates/temp.py
+++ all_tasks/static/templates/temp.py
@@ -17,3 +17,24 @@ proposal = """
 </body>
 </html>
 """
+
+result = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Варианты выбора</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="http://localhost:8080/static/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Результаты отбора</h1>
+        <h2>Претендента на участие в миссии {}:</h2>
+        <div class="col green">Поздравляем! Ваш рейтинг после {} этапа отбора</div>
+        <div class="col coral">составляет {}!</div>
+        <div class="col yellow">Удачи!</div>
+    </div>
+</body>
+</html>
+"""
